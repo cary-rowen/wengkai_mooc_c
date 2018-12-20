ee8f421b5abdf965f88ff181550b4317dce0f92c
许叶杨 <xuyeyang@outlook.com>
2018-12-20 20:52:12 +0800

    更新到第6周

--- Week07-02-find_point.c
+++ Week07-02-find_point.c
@@ -22,50 +22,14 @@ int main() {
                 matrix[i][j] = 0;
 
         for (int i = 0; i < n; i++) {
-            int is_num = 0;
-            int cnt_space = 0;
+            getchar();
             for (int j = 0; j < n; j++) {
-                int c = getchar();
+                char c = getchar();
 
-                if (c == ' ')
-                    cnt_space ++;
-
-
-                /*
-                 *
-                 * 4
-32 43 45 234
-34 4 6 33 23
-0 9 3 8
-32 9 2 34 543
-
-32 43 45 2
-34 34 4 6
-33 23 0 9
-3 8 32 9
-
-                 *
-                 */
-
-                if (is_num && (c - '0' >= 0 && c - '0' < 10)) {
-                    j --;
-                    matrix[i][j] = matrix[i][j] * 10 + (c - '0' + 0);
-                    continue;
-                }
-
-                if (c == ' ' || c == '\n') {
-                    j --;
-                    cnt_space ++;
-                    is_num = 0;
-                    continue;;
-                }
-
-                is_num = 1;;
-
-                matrix[i][j] = c - '0' + 0;
             }
         }
 
+        printf("\n==========\n");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++)
                 printf("%d ", matrix[i][j]);
